dcadbbea32e7c6e6b04b619d5225810fb34e0d89
vaibhav chauhan <vaibhav@ixsystems.com>
2016-01-09 22:29:23 -0800

    adding python executable as a tool for renaming boot-env

---
+++ gui/tools/rename.py
@@ -0,0 +1,9 @@
+#usr/local/bin/python
+
+import subprocess
+version_running = subprocess.check_output(['cat','/etc/version'])
+version_running = version_running.rstrip('\n')
+version_string = version_running.split('-')[0]
+version_number = version_running.split('-')[-1]
+boot_env = version_string+'-'+version_string+'-'+version_number
+subprocess.check_output(['beadm','rename',boot_env,version_running])
