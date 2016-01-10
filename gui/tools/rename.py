258a42916643f5334aec7439f89ade20491760e6
vaibhav chauhan <vaibhav@ixsystems.com>
2016-01-09 23:59:17 -0800

    added try except block

--- gui/tools/rename.py
+++ gui/tools/rename.py
@@ -1,9 +1,14 @@
 #usr/local/bin/python
 
 import subprocess
+
 version_running = subprocess.check_output(['cat','/etc/version'])
 version_running = version_running.rstrip('\n')
 version_string = version_running.split('-')[0]
 version_number = version_running.split('-')[-1]
 boot_env = version_string+'-'+version_string+'-'+version_number
-subprocess.check_output(['beadm','rename',boot_env,version_running])
+try:
+    subprocess.check_output(['beadm','rename',boot_env,version_running])
+except:
+    print "boot enviorment {0} does not exists".format(boot_env)
+        
