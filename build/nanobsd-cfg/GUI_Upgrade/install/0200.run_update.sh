12dad2b4ff1dbc22fb1da94eb6eb5904c02f10f5
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-02-11 03:24:30 -0800

    This change allow us to start using TrueNAS-9.3.*-RELEASE versioning scheme.internal STABLE builds will be seen in TrueNAS-9.3-STABLE-YYYYMMDDHHMM format. external TN SU's will be seen as TrueNAS-9.3.*-RELEASE.
    this diffrence was made to clearly distinguish between external
    and internal STABLE SU's.

--- build/nanobsd-cfg/GUI_Upgrade/install/0200.run_update.sh
+++ build/nanobsd-cfg/GUI_Upgrade/install/0200.run_update.sh
@@ -52,10 +52,15 @@ standard_upgrade()
 	# place, so we only have to be concerned with gui-packages.tar.
     set -x
 	OS=${AVATAR_PROJECT}
+    UPDATE_INTERNAL=${UPDATE_INTERNAL}
 	UPGRADE_DIR=${SCRIPTDIR}/update
 	mkdir -p ${UPGRADE_DIR} || upgrade_fail "Unable to create package directory"
 	tar xf ${SCRIPTDIR}/gui-packages.tar -C ${UPGRADE_DIR} || upgrade_fail "Unable to extract package files"
+    if [" ${UPDATE_INTERNAL}" = "yes" ]; then
 	NEW_VERSION=${OS}-$(/usr/local/bin/manifest_util -M ${UPGRADE_DIR}/${OS}-MANIFEST sequence)
+    else
+    NEW_VERSION=$(/usr/local/bin/manifest_util -M ${UPGRADE_DIR}/${OS}-MANIFEST sequence)
+    fi
 	if [ "${NEW_VERSION}" = "${OS}-" ]; then
 	    upgrade_fail "Unable to determine sequence for new version"
 	fi
